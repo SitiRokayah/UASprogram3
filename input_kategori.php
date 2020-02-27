cb9dde9dec92816764a22f31ec270e4a9f09cc0e
SitiRokayah <56638582+SitiRokayah@users.noreply.github.com>
2020-02-27 17:45:25 +0700

    Add files via upload

---
+++ input_kategori.php
@@ -0,0 +1,33 @@
+<?php
+include("koneksi.php"); //fungsi untuk gabung file
+if(isset($_POST['simpan'])){
+	$query_tambah_data="insert into barang(id_barang,nama,kategori_id,satuan_id)values
+	('".$_POST['id_kategori']."',
+	 '".$_POST['nama']."')";
+$proses_data=mysqli_query($rumah,$query_tambah_data);
+if($proses_data){
+	echo 'tambah data berhasil';
+}else{
+	echo mysqli_error($rumah);
+}
+
+}
+?>
+
+
+<form method="POST" action=""> <!---form untuk kirim data ke DB-->
+<table border="1"> <!--nama harus sama dengan database-->
+	<tr>
+		<td>id_kategori</td>
+		<td><input name="id_kategori" type="text"></td>
+	</tr>
+	<tr>
+		<td>nama</td>
+		<td><input name="nama" type="text"></td>
+	</tr>
+	<tr>
+		<td><input name="simpan" type="submit"></td>
+	</tr>
+</table>
+</form>
+		
